ec198fdde1f8e9af9ea8b165a6ac79f189e13bac
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-06-28 16:44:57 +0200

    added: Power Shell commands.txt modified: tests/first_tests/first_tests.rs 20250628 1643 CET SDvW

--- bad_singly_linked_stack/tests/first_tests.rs
+++ bad_singly_linked_stack/tests/first_tests.rs
@@ -1,30 +1,34 @@
 use bad_singly_linked_stack::first::List;
 
 #[test]
-fn basics() {
+fn check_empty_list_behaves_right() {
     let mut list = List::new();
-
-    // Check empty list behaves right
     assert_eq!(list.pop(), None);
+}
 
-    // Push some elements
+#[test]
+fn check_normal_removal() {
+    let mut list = List::new();
     list.push(1);
     list.push(2);
     list.push(3);
-
-    // Check normal removal
     assert_eq!(list.pop(), Some(3));
     assert_eq!(list.pop(), Some(2));
+}
 
-    // Push some more just to make sure nothing's corrupted
+#[test]
+fn check_push_more() {
+    let mut list = List::new();
     list.push(4);
     list.push(5);
-
-    // Check normal removal
     assert_eq!(list.pop(), Some(5));
     assert_eq!(list.pop(), Some(4));
+}
 
-    // Check exhaustion
+#[test]
+fn check_exhaustion() {
+    let mut list = List::new();
+    list.push(1);
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.pop(), None);
 }
